4d97da1f92d333f6f932dff66ab4cd5e64e4872f
fuzenblue <primrosemprst@gmail.com>
2026-02-10 21:12:36 +0700

    feat: Initialize client-side application with core components, routing, and an API service.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
 import ChatPage from './pages/ChatPage'
 import MapPage from './pages/MapPage'
@@ -8,12 +10,14 @@ import DatabasePage from './pages/DatabasePage'
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/map" element={<MapPage />} />
         <Route path="/database" element={<DatabasePage />} />
       </Routes>
+      <Footer />
     </Router>
   )
 }
